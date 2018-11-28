2f2a84f6153ee81b5519eca92df0e5c985aa07b0
Andres Correa Casablanca <andres@thirdhash.com>
2018-11-28 19:59:47 +0100

    Add test for utils.graph.weighted_random_int
    Signed-off-by: Andres Correa Casablanca <andres@thirdhash.com>

--- test/experiments/utils/test_graph.py
+++ test/experiments/utils/test_graph.py
@@ -5,9 +5,34 @@
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 
-from experiments.utils.graph import degrees_distribution
+from collections import defaultdict
+
+from experiments.utils.graph import (
+    degrees_distribution,
+    weighted_random_int
+)
 
 
 def test_degrees_distribution():
     assert [1, 3, 6, 10, 15, 21] == degrees_distribution([1, 2, 3, 4, 5, 6])
     assert [6, 11, 15, 18, 20, 21] == degrees_distribution([6, 5, 4, 3, 2, 1])
+
+
+def test_weighted_random_int():
+    range5 = list(range(5))
+
+    counter = defaultdict(int)
+    for i in range(10000):
+        rnd = weighted_random_int([1, 12, 25, 50, 100])
+        counter[rnd] += 1
+        assert rnd in range5
+    for i in range(1, 5):
+        assert counter[i - 1] < counter[i]
+
+    counter = defaultdict(int)
+    for i in range(10000):
+        rnd = weighted_random_int([50, 75, 88, 99, 100])
+        counter[rnd] += 1
+        assert rnd in range5
+    for i in range(1, 5):
+        assert counter[i - 1] > counter[i]
