cfdc13c1be125f9d337e95aad2385db0f2002a72
Andres Correa Casablanca <andres@thirdhash.com>
2018-11-29 16:07:21 +0100

    Use relative dirs in .travis/02_script.sh
    Signed-off-by: Andres Correa Casablanca <andres@thirdhash.com>

--- .travis/02_script.sh
+++ .travis/02_script.sh
@@ -4,7 +4,10 @@
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-source ".venv/bin/activate"
+TRAVIS_SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PROJECT_DIR="$( cd "${TRAVIS_SCRIPTS_DIR}/.." && pwd )"
+
+source "${PROJECT_DIR}/.venv/bin/activate"
 export $(egrep -v '^#' .env | xargs -d '\n')
 
 pytest
