d395a1788aa5853846e1363d0680ebe11065b434
Andres Correa Casablanca <andres@thirdhash.com>
2018-11-30 11:48:08 +0100

    Remove brances around env vars
    Signed-off-by: Andres Correa Casablanca <andres@thirdhash.com>

--- .travis/00_before_install.sh
+++ .travis/00_before_install.sh
@@ -5,4 +5,4 @@
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 export TRAVIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-openssl aes-256-cbc -K ${encrypted_3508967e0358_key} -iv ${encrypted_3508967e0358_iv} -in "${TRAVIS_DIR}/ssh_key.enc" -out "${TRAVIS_DIR}/ssh_key" -d
+openssl aes-256-cbc -K $encrypted_3508967e0358_key -iv $encrypted_3508967e0358_iv -in "${TRAVIS_DIR}/ssh_key.enc" -out "${TRAVIS_DIR}/ssh_key" -d
