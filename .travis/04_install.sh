ce0b092bcd6b21cedb280f06fce84d5082e0b71f
Andres Correa Casablanca <andres@thirdhash.com>
2018-11-30 14:20:48 +0100

    Move PEP 508 check step
    Signed-off-by: Andres Correa Casablanca <andres@thirdhash.com>

--- .travis/04_install.sh
+++ .travis/04_install.sh
@@ -10,5 +10,6 @@ export PIPENV_IGNORE_VIRTUALENVS=1  # Use own virtualenv instead of Travis' one
 
 pip install pipenv
 pipenv install --dev
+pipenv check  # Checking potential dependency problems (PEP 508 requirements)
 
 git clone git@github.com:dtr-org/unit-e.git "${TRAVIS_DIR}/unit-e"
