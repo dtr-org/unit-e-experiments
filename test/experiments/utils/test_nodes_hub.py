9595eec0e5338aa5221ffef0f30e055ccbfefd82
Andres Correa Casablanca <andres@thirdhash.com>
2019-01-31 10:21:35 +0100

    Test NodesHub.wait_for_pending_connections
    Signed-off-by: Andres Correa Casablanca <andres@thirdhash.com>

--- test/experiments/utils/test_nodes_hub.py
+++ test/experiments/utils/test_nodes_hub.py
@@ -1,17 +1,21 @@
-from asyncio import AbstractEventLoop, AbstractServer
+from asyncio import (
+    AbstractEventLoop,
+    AbstractServer,
+    sleep as asyncio_sleep
+)
 from os import getpid
 from subprocess import Popen
-from unittest.mock import Mock
+from unittest.mock import Mock, patch
 
 import pytest
-
 import test_framework.util as tf_util
 
+from asynctest.mock import CoroutineMock
 from test_framework.test_node import TestNode as FakeNode
 
 from experiments.utils.latencies import LatencyPolicy
 from experiments.utils.networking import get_pid_for_local_port
-from experiments.utils.nodes_hub import NodesHub
+from experiments.utils.nodes_hub import NodesHub, NUM_OUTBOUND_CONNECTIONS
 
 
 def test_get_port_methods():
@@ -36,7 +40,7 @@ def test_get_port_methods():
 
 
 @pytest.mark.asyncio
-async def test_start_proxies(event_loop):
+async def test_start_proxies(event_loop: AbstractEventLoop):
     init_environment()
 
     nodes_hub = NodesHub(
@@ -61,6 +65,52 @@ async def test_start_proxies(event_loop):
         ))
 
     nodes_hub.close()
+
+
+@pytest.mark.asyncio
+async def test_wait_for_pending_connections(event_loop: AbstractEventLoop):
+    init_environment()
+
+    async def fake_sleep(_delay):
+        await asyncio_sleep(0)
+
+    with patch(
+        target='experiments.utils.nodes_hub.asyncio_sleep',
+        new=fake_sleep  # We avoid sleeping, but need to switch context
+    ), patch(
+        target='experiments.utils.nodes_hub.NodesHub.connect_sender_to_proxy',
+        new=CoroutineMock(spec=NodesHub.connect_sender_to_proxy)
+    ):
+        nodes_hub = NodesHub(
+            loop=event_loop,
+            latency_policy=Mock(spec=LatencyPolicy),
+            nodes=[get_node_mock(node_id) for node_id in range(5)]
+        )
+        # nodes_hub.connect_sender_to_proxy = fake_connect_sender_to_proxy
+
+        # 1st: Blocked because we still have to try establishing connections
+        wait_task = event_loop.create_task(
+            nodes_hub.wait_for_pending_connections()  # SUT
+        )
+        await asyncio_sleep(0)  # Switching context
+        assert (not wait_task.done())
+        nodes_hub.num_connection_intents = 5 * NUM_OUTBOUND_CONNECTIONS
+        await asyncio_sleep(0)  # Switching context
+        assert (wait_task.done())
+
+        # 2nd: Blocked because we tried to connect, and we're waiting for results
+        nodes_hub.pending_connections = {(0, 1), (1, 2), (2, 3)}
+        wait_task = event_loop.create_task(
+            nodes_hub.wait_for_pending_connections()  # SUT
+        )
+        await asyncio_sleep(0)  # Switching context
+        assert (not wait_task.done())
+        nodes_hub.pending_connections = set()
+        for _ in range(10):
+            await asyncio_sleep(0)  # Switching context
+        assert (wait_task.done())
+
+        nodes_hub.close()
 
 
 def init_environment():
