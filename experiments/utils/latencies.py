347668c6d157f881c38a12f0ca82e4a93ef5303f
Andres Correa Casablanca <andres@thirdhash.com>
2019-02-01 18:20:40 +0100

    Add ExponentiallyDistributedLatencyPolicy class
    Signed-off-by: Andres Correa Casablanca <andres@thirdhash.com>

--- experiments/utils/latencies.py
+++ experiments/utils/latencies.py
@@ -6,6 +6,7 @@
 
 
 from abc import ABCMeta, abstractmethod
+from random import expovariate
 from typing import Dict, Optional, Tuple
 
 
@@ -21,7 +22,7 @@ class StaticLatencyPolicy(LatencyPolicy):
         self.node2node_delays: Dict[Tuple[int, int], float] = {}
 
     def set_delay(self, src_node: int, dst_node: int, delay: Optional[float]):
-        if delay is None or delay == self.base_delay:
+        if delay is None:
             self.node2node_delays.pop((src_node, dst_node), None)
         else:
             self.node2node_delays[(src_node, dst_node)] = delay
@@ -30,3 +31,28 @@ class StaticLatencyPolicy(LatencyPolicy):
         if (src_node, dst_node) in self.node2node_delays:
             return self.node2node_delays[(src_node, dst_node)]
         return self.base_delay
+
+
+class ExponentiallyDistributedLatencyPolicy(LatencyPolicy):
+    def __init__(self, avg_delay: float = 0.0):
+        self.avg_delay = avg_delay
+        self.node2node_avg_delays: Dict[Tuple[int, int], float] = {}
+
+    def set_avg_delay(
+            self, src_node: int, dst_node: int, avg_delay: Optional[float]
+    ):
+        if avg_delay is None:
+            self.node2node_avg_delays.pop((src_node, dst_node), None)
+        else:
+            self.node2node_avg_delays[(src_node, dst_node)] = avg_delay
+
+    def get_delay(self, src_node: int, dst_node: int) -> float:
+        if (src_node, dst_node) in self.node2node_avg_delays:
+            avg_delay = self.node2node_avg_delays[(src_node, dst_node)]
+        else:
+            avg_delay = self.avg_delay
+
+        if avg_delay == 0.0:
+            return 0.0
+
+        return expovariate(1.0 / avg_delay)
